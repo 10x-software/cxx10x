e83c12190705843ff15d3893ec5d7486c310f963
IAP <27013475+ilya-pevzner@users.noreply.github.com>
2025-02-20 00:20:32 -0500

    fix mongo pipeline generation, update test

--- infra_10x/tests/test.py
+++ infra_10x/tests/test.py
@@ -10,6 +10,6 @@ pipeline = []
 data = dict(serialized_traitable)
 MongoCollectionHelper.prepare_filter_and_pipeline(data, filter, pipeline)
 
-assert data == serialized_traitable
+assert data|filter == serialized_traitable
 assert filter == {'_id': 'AAA', '_rev': 10}
-assert pipeline == [{'$replaceRoot': {'newRoot': {'_id': 'AAA', '_rev': {'$cond': [{'$and': [{'$eq': ['$_id', {'$literal': 'AAA'}]}, {'$eq': ['$_rev', {'$literal': 10}]}, {'$eq': ['$name', {'$literal': 'test'}]}, {'$eq': ['$age', {'$literal': 60}]}]}, 10, 11]}}}}, {'$replaceWith': {'$setField': {'field': '_id', 'input': '$$ROOT', 'value': {'$literal': 'AAA'}}}}, {'$replaceWith': {'$setField': {'field': '_rev', 'input': '$$ROOT', 'value': {'$literal': 10}}}}, {'$replaceWith': {'$setField': {'field': 'name', 'input': '$$ROOT', 'value': {'$literal': 'test'}}}}, {'$replaceWith': {'$setField': {'field': 'age', 'input': '$$ROOT', 'value': {'$literal': 60}}}}]
+assert pipeline == [{'$replaceRoot': {'newRoot': {'_id': 'AAA', '_rev': {'$cond': [{'$and': [{'$eq': ['$name', {'$literal': 'test'}]}, {'$eq': ['$age', {'$literal': 60}]}]}, 10, 11]}}}}, {'$replaceWith': {'$setField': {'field': 'name', 'input': '$$ROOT', 'value': {'$literal': 'test'}}}}, {'$replaceWith': {'$setField': {'field': 'age', 'input': '$$ROOT', 'value': {'$literal': 60}}}}]
