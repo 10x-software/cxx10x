a1fbbe1b678b25d99ac79528ae1b0d6d3bd26473
IAP <27013475+ilya-pevzner@users.noreply.github.com>
2025-02-19 20:34:37 -0500

    enhance test

--- core_10x/tests/test.py
+++ core_10x/tests/test.py
@@ -1,7 +1,11 @@
 from core_10x.code_samples.person import Person
 
 p = Person(first_name = 'Sasha', last_name = 'Davidovich')
+assert p._rev == 0
 assert p.id() == 'Sasha|Davidovich'
+print(p.full_name)
+print(p.serialize(False))
+print(p.serialize(True))
 
 p1 = Person(first_name='John', last_name='Doe', age=30)
 assert p1.id() == 'John|Doe'
