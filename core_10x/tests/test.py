a8ba56568c9ee2076475bc4d138e02c467867a3d
ilya-pevzner <pevzner@nyu.edu>
2025-02-20 14:22:05 -0500

    fix tests, add run configs for debugging

--- core_10x/tests/test.py
+++ core_10x/tests/test.py
@@ -1,11 +1,15 @@
 from core_10x.code_samples.person import Person
+if __name__ == '__main__':
+    from core_10x.code_samples.person import Person
+    from core_10x.ts_union import TsUnion
 
-p = Person(first_name = 'Sasha', last_name = 'Davidovich')
-assert p._rev == 0
-assert p.id() == 'Sasha|Davidovich'
-print(p.full_name)
-print(p.serialize(False))
-print(p.serialize(True))
+    with TsUnion():
+        p = Person(first_name = 'Sasha', last_name = 'Davidovich')
+        assert p._rev == 0
+        assert p.id() == 'Sasha|Davidovich'
+        print(p.full_name)
+        print(p.serialize(False))
+        print(p.serialize(True))
 
-p1 = Person(first_name='John', last_name='Doe', age=30)
-assert p1.id() == 'John|Doe'
+        p1 = Person(first_name='John', last_name='Doe', age=30)
+        assert p1.id() == 'John|Doe'
