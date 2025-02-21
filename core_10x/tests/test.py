de664b6c53a04016822d6fead3e797f77e1272eb
ilya-pevzner <pevzner@nyu.edu>
2025-02-21 17:58:32 -0500

    cleanup tests

--- core_10x/tests/test.py
+++ core_10x/tests/test.py
@@ -1,15 +1,16 @@
+from core_10x.ts_union import TsUnion
 from core_10x.code_samples.person import Person
+
 if __name__ == '__main__':
-    from core_10x.code_samples.person import Person
-    from core_10x.ts_union import TsUnion
 
     with TsUnion():
         p = Person(first_name = 'Sasha', last_name = 'Davidovich')
         assert p._rev == 0
         assert p.id() == 'Sasha|Davidovich'
-        print(p.full_name)
-        print(p.serialize(False))
-        print(p.serialize(True))
+        # print(p.full_name)
+        # print(p.serialize(False))
+        # print(p.serialize(True))
 
-        p1 = Person(first_name='John', last_name='Doe', age=30)
-        assert p1.id() == 'John|Doe'
+        # p1 = Person(first_name='John', last_name='Doe', age=30)
+        # assert p1.id() == 'John|Doe'
+        # print(p1.full_name)
