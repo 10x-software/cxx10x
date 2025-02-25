79d19fd8cc1dc50f2db9b878f90b41cdafbfc109
AMD <alexander.m.davidovich@gmail.com>
2025-02-25 10:06:19 +0400

    + BUiNode

--- core_10x/bnode.h
+++ core_10x/bnode.h
@@ -33,8 +33,8 @@ public:
     BasicNode() : m_value(PyLinkage::XNone()), m_state(0x0)  {}
     virtual ~BasicNode() = default;
 
-    [[nodiscard]] bool is_valid() const { return m_state & STATE_VALID; }
-    [[nodiscard]] bool is_set() const   { return m_state == STATE_VALID_AND_SET; }
+    [[nodiscard]] bool is_valid() const         { return m_state & STATE_VALID; }
+    [[nodiscard]] bool is_set() const           { return m_state == STATE_VALID_AND_SET; }
     [[nodiscard]] bool is_valid_and_not_set() const { return (m_state & STATE_VALID) && ((m_state & STATE_SET) == 0x0); }
 
     [[nodiscard]] py::object value() const      { return m_value; }
@@ -44,9 +44,17 @@ public:
     void assign_value(const py::object& v)      { m_value = v; }
     void assign(const py::object& v)            { assign_value(v); m_state |= STATE_VALID; }
     void set_imported(const py::object& v)      { set(v); m_state |= STATE_IMPORTED; }
+    void make_valid()                           { m_state |= STATE_VALID; }
+    void make_invalid()                         { m_state &= ~STATE_VALID_AND_SET; }
 
     virtual void set(const py::object& v )      { m_value = v; m_state = STATE_VALID_AND_SET; }
-    virtual void invalidate()                   { m_value = PyLinkage::XNone(); m_state &= ~STATE_VALID_AND_SET; }
+
+    virtual void invalidate() {
+        if (is_valid()) {
+            m_value = PyLinkage::XNone();
+            m_state &= ~STATE_VALID_AND_SET;
+        }
+    }
 
     virtual NodeSet* children()                 { return nullptr; }
     virtual void clear_children()               {}
@@ -81,7 +89,14 @@ public:
                 p->invalidate();
     }
 
-    void invalidate() override                  { BasicNode::invalidate(); invalidate_parents(); }
+    void invalidate() final {
+        if (is_valid()) {
+            m_value = PyLinkage::XNone();
+            m_state = 0x0;
+            invalidate_parents();
+        }
+    }
+
     void set(const py::object& value) override  { BasicNode::set(value); invalidate_parents(); }
 
     NodeSet* parents()  override                 { return &m_parents; }
@@ -150,4 +165,69 @@ public:
     //[[nodiscard]] BTrait* trait() const               { return m_trait; }
     //[[nodiscard]] TID* tid() const                    { return m_tid; }
 
+};
+
+class BUiNode : public BasicNode {
+    py::object  m_trait_widget;
+    py::object  f_refresh_emit;     //-- lambda trait_widget, ui_node: py_ui_node.REFRESH.emit(trait_widget, ui_node)
+    BasicNode*  m_entity_node;
+    BasicNode*  m_trait_node;
+    BasicNode*  m_sheet_node;
+
+public:
+    BUiNode(py::object trait_widget, py::object refresh_emit, BasicNode* entity_node, BasicNode* trait_node, BasicNode* sheet_node) : BasicNode() {
+        m_trait_widget = trait_widget;
+        f_refresh_emit = refresh_emit;
+        m_entity_node = entity_node;
+        m_trait_node = trait_node;
+        m_sheet_node = sheet_node;
+
+        m_state = STATE_VALID;
+
+        entity_node->add_parent(this);
+        trait_node->add_parent(this);
+        if (sheet_node)
+            sheet_node->add_parent(this);
+    }
+
+    void relink_nodes(BasicNode* entity_node, BasicNode* trait_node, BasicNode* sheet_node) {
+        if (m_entity_node != entity_node) {
+            m_entity_node->remove_parent(this);
+            entity_node->add_parent(this);
+            m_entity_node = entity_node;
+        }
+        if (m_trait_node != trait_node) {
+            m_trait_node->remove_parent(this);
+            trait_node->add_parent(this);
+            m_trait_node = trait_node;
+        }
+        if (sheet_node and sheet_node != m_sheet_node) {
+            if (m_sheet_node)
+                m_sheet_node->remove_parent(this);
+            sheet_node->add_parent(this);
+            m_sheet_node = sheet_node;
+        }
+    }
+
+    void invalidate() final {
+        if (is_valid()) {
+            m_state = 0x0;
+            f_refresh_emit(m_trait_widget, this);
+        }
+    }
+
+    void unlink() final {
+        if (m_entity_node) {
+            m_entity_node->remove_parent(this);
+            m_entity_node = nullptr;
+        }
+        if (m_trait_node) {
+            m_trait_node->remove_parent(this);
+            m_trait_node = nullptr;
+        }
+        if (m_sheet_node) {
+            m_sheet_node->remove_parent(this);
+            m_sheet_node = nullptr;
+        }
+    }
 };
