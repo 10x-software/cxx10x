412d900a0738612dd4cf71e13c27d32aea47d58f
AMD <alexander.m.davidovich@gmail.com>
2025-02-27 09:06:46 +0400

    - removed mutex from cache; - changed semantics of BTraitable::initialize() as discussed; - changed set_values() signature as discussed

--- core_10x/simple_cache_layer.cpp
+++ core_10x/simple_cache_layer.cpp
@@ -19,10 +19,12 @@ ObjectCache* SimpleCacheLayer::find_or_create_object_cache(const TID &tid) {
     if (oc)
         return oc;
 
-    {
-        std::shared_lock guard(*m_parent->shared_mutex());
-        oc = m_parent->find_object_cache(tid);
-    }
+    //-- AMD-1 //
+    //{
+    //    std::shared_lock guard(*m_parent->shared_mutex());
+    //    oc = m_parent->find_object_cache(tid);
+    //}
+    oc = m_parent->find_object_cache(tid);
 
     if (!oc)    //-- the object cache is not there, we have a lazy tid reference - let's load the object
         tid.cls()->load(tid.id(), true);
@@ -32,7 +34,7 @@ ObjectCache* SimpleCacheLayer::find_or_create_object_cache(const TID &tid) {
 }
 
 BasicNode* SimpleCacheLayer::find_or_create_node(const TID& tid, BTrait* trait, int node_type) {
-    std::unique_lock guard(m_rw_mutex);
+    //-- AMD-1 //std::unique_lock guard(m_rw_mutex);
 
     auto oc = find_or_create_object_cache(tid);
     auto node = oc->find_node(trait);
@@ -49,7 +51,7 @@ BasicNode* SimpleCacheLayer::find_or_create_node(const TID& tid, BTrait* trait, 
 }
 
 BasicNode* SimpleCacheLayer::find_or_create_node(const TID& tid, BTrait* trait, const py::args& args, int node_type) {
-    std::unique_lock guard(m_rw_mutex);
+    //-- AMD-1 //std::unique_lock guard(m_rw_mutex);
 
     auto oc = find_or_create_object_cache(tid);
     auto node = oc->find_node(trait, args);
@@ -67,8 +69,8 @@ BasicNode* SimpleCacheLayer::find_or_create_node(const TID& tid, BTrait* trait, 
 
 //-- Export all BASIC_GRAPH nodes set in this layer
 void SimpleCacheLayer::export_nodes() {
-    std::shared_lock read_guard(m_rw_mutex);
-    std::unique_lock write_guard(*m_parent->shared_mutex());
+    //-- AMD-1 //std::shared_lock read_guard(m_rw_mutex);
+    //-- AMD-1 //std::unique_lock write_guard(*m_parent->shared_mutex());
 
     auto dst_node_type = m_parent->default_node_type();
     for (const auto& oc_item : m_data) {
