af9935be3f81e05fbffa7376ffbab6baf7b31b83
AMD <78048725+sasha-davidovich@users.noreply.github.com>
2025-03-02 15:23:19 +0400

    cp - fixing get_style_sheet()

--- core_10x/btrait.cpp
+++ core_10x/btrait.cpp
@@ -135,6 +135,9 @@ py::object BTrait::wrapper_f_choices(BTraitable *obj) {
 }
 
 py::object BTrait::wrapper_f_style_sheet(BTraitable *obj)  {
+    if (!f_style_sheet)
+        return PyLinkage::empty_str();
+
     try {
         return f_style_sheet(obj);
     } catch (py::error_already_set& exc) {
